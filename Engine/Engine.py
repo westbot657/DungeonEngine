458cb6419f39b1eaedb34001c4e33c2ac125cc7e
Weston Day <82739021+westbot657@users.noreply.github.com>
2023-01-06 17:26:05 +0000

    engine

--- Engine/Engine.py
+++ Engine/Engine.py
@@ -1,7 +1,19 @@
 # pylint: disable=[W,R,C,import-error]
+from Resources.AbstractAmmo         import AbstractAmmo
+from Resources.AbstractArmor        import AbstractArmor
+from Resources.AbstractItem         import AbstractItem
+from Resources.AbstractStatusEffect import AbstractStatusEffect
+from Resources.AbstractWeapon       import AbstractWeapon
+
+
 
 class Engine:
-    ...
+    _engine = None
+    def __new__(cls):
+        if not cls._engine:
+            cls._engine = self = super().__new__(cls)
+            
+        return cls._engine
 
 
 
---
+++ Engine/Resources/AbstractCombat.py
@@ -0,0 +1,43 @@
+# pylint: disable=[W,R,C,import-error]
+
+try:
+    from .Combat import Combat
+    from .Identifier import Identifier
+except ImportError:
+    from Combat import Combat
+    from Identifier import Identifier
+
+import glob, json, re
+
+
+class AbstractCombat:
+    _loaded: dict = {}
+
+    def __init__(self, identifier:Identifier, data:dict):
+        self.identifier = identifier
+        
+    @classmethod
+    def loadData(cls) -> list:
+        files: list[str] = glob.glob("**/combats/*.json", recursive=True)
+
+        for file in files:
+            file: str
+            with open(file, "r+", encoding="utf-8") as f:
+                data = json.load(f)
+            
+            if m := re.match(r"Dungeons/(?P<namespace>[^/]+)/resources/(?P<path>(?:[^/]+/)+)(?P<name>[a-z0-9_]+)\.json", file):
+                d: dict = m.groupdict()
+                namespace:str = d["namespace"]
+                path: str = f"Dungeons/{namespace}/resources/{d['path']}"
+                name: str = d["name"]
+                cls._loaded.update({f"{namespace}:combats/{name}": cls(Identifier(namespace, path, name), data)})
+
+            elif m := re.match(r"resources/combats/(?P<name>[a-z0-9_]+)\.json", file):
+                d: dict = m.groupdict()
+                name: str = d["name"]
+                cls._loaded.update({f"engine:combats/{name}": cls(Identifier("engine", "resources/combats/", name), data)})
+
+
+        return cls._loaded
+
+

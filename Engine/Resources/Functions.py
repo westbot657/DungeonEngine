779299fbf999aab6664e54d2f627a1786a97f872
Weston Day <82739021+westbot657@users.noreply.github.com>
2023-01-15 01:44:08 +0000

    drtfgvybhunjimk

--- Engine/Resources/Functions.py
+++ Engine/Resources/Functions.py
@@ -12,6 +12,10 @@ import random, math, re
 """
 functions:
 engine:tool/cancel_use
+engine:tool/get_durability
+engine:tool/set_durability
+engine:tool/set_max_durability
+engine:tool/get_max_durability
 
 engine:random/uniform
 engine:random/weighted
@@ -19,6 +23,8 @@ engine:random/weighted
 engine:player/message
 engine:player/give_item
 engine:player/get_status_effect
+enigne:player/give_status_effect
+engine:player/has_status_effect
 engine:player/get_health
 engine:player/get_max_health
 engine:player/heal
@@ -28,9 +34,31 @@ engine:player/get_equipped_armor
 engine:player/has_item
 engine:player/get_item
 
-
 engine:status_effect/get_level
+engine:status_effect/set_level
 engine:status_effect/get_duration
+engine:status_effect/set_duration
+
+engine:weapon/get_durability
+engine:weapon/set_durability
+engine:weapon/get_max_durability
+engine:weapon/set_max_durability
+engine:weapon/get_damage
+engine:weapon/set_damage
+engine:weapon/get_parent_type
+engine:weapon/get_ammo_type
+
+engine:item/get_count
+engine:item/set_count
+engine:item/get_max_count
+engine:item/set_max_count
+
+engine:armor/get_durability
+engine:armor/set_durability
+engine:armor/get_damage_reduction
+engine:armor/set_damage_reduction
+engine:armor/get_max_durability
+engine:armor/set_max_durability
 
 
 """

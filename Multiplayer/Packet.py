b350825e181dc6749dd1844a14b72ca78a040b71
Weston Day <82739021+westbot657@users.noreply.github.com>
2023-02-22 00:45:11 +0000

    stuff

--- Multiplayer/Packet.py
+++ Multiplayer/Packet.py
@@ -3,16 +3,31 @@ import socket
 
 class Packet:
     _content_builders = {}
+    _queue = []
 
     def __init__(self, source, content):
         self.source = source
         self.content = content
 
     @classmethod
-    def build(cls, source:str, content:str):
-        if source not in cls._content_builders:
-            cls._content_builders.update({source: ""})
-        cls._content_builders[source] += content
+    def build(cls, source:socket.socket, address:str, content:str):
+        if address not in cls._content_builders:
+            cls._content_builders.update({address: ""})
+        cls._content_builders[address] += content
 
-        if ">" in (text := cls._content_builders[source]):
-            ...
+        while "\n" in (text := cls._content_builders[address]):
+            packet_content, text = text.split("\n", 1)
+            packet_content = packet_content.replace("&new;", "\n").replace("&amp;", "&")
+            cls._content_builders[address] = text
+            packet = cls(source, content)
+
+            cls._queue.append(packet)
+
+    @classmethod
+    def nextPacket(cls):
+        if cls._queue:
+            return cls._queue.pop(0)
+        return None
+
+
+

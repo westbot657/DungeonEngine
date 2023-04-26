9b1e7efeef6385fc8f7157ce7198ce1dcbe9bf6e
Weston Day <82739021+westbot657@users.noreply.github.com>
2023-04-26 16:31:29 +0000

    Starting to add Environment, so that rooms can be made, and predicates can work

---
+++ Engine/Resources/Environment.py
@@ -0,0 +1,14 @@
+# pylint: disable=[W,R,C,import-error]
+
+
+# This is used to store info on a location like:
+# humidity
+# temperature
+# what can live where
+# other descriptors, made as needed
+
+class Environment:
+
+    def __init__(self):
+        ...
+

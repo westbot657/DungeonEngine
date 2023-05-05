4762b6b1f1482fa846961a16cb767781462f6234
Weston Day <82739021+westbot657@users.noreply.github.com>
2023-05-05 14:50:07 +0000

    also starting to set up events for other game objects

--- Engine/Resources/Armor.py
+++ Engine/Resources/Armor.py
@@ -5,20 +5,23 @@ try:
     from .Identifier import Identifier
     from .DynamicValue import DynamicValue
     from .Util import Util
+    from .FunctionMemory import FunctionMemory
 except ImportError:
     from GameObject import GameObject
     from Identifier import Identifier
     from DynamicValue import DynamicValue
     from Util import Util
+    from FunctionMemory import FunctionMemory
 
 class Armor(GameObject):
     identifier = Identifier("engine", "object/", "armor")
-    def __init__(self, abstract, name:str, damage_reduction:DynamicValue, max_durability:int, durability:int):
+    def __init__(self, abstract, name:str, damage_reduction:DynamicValue, max_durability:int, durability:int, events:dict):
         self.abstract = abstract
         self.name = name
         self.damage_reduction = damage_reduction
         self.max_durability = max_durability
         self.durability = durability
+        self.events = events
 
         self.owner = None
 
@@ -31,3 +34,44 @@ class Armor(GameObject):
     def fullStats(self, engine, is_equipped=False):
         return f"{self.name} +{self.damage_reduction.fullDisplay(engine)}def {Util.getDurabilityBar(self.durability, self.max_durability)}" + (" [WEARING]" if is_equipped else "")
 
+    def getLocalVariables(self) -> dict:
+        d = {}
+
+        return d
+
+    def updateLocalVariables(self, locals: dict):
+        ...
+    
+    def prepFunctionMemory(self, function_memory:FunctionMemory):
+        function_memory.addContextData({
+            "#armor": self
+        })
+        function_memory.update(self.getLocalVariables())
+
+    def postEvaluate(self, function_memory:FunctionMemory):
+        self.updateLocalVariables(function_memory.symbol_table)
+
+
+    def onEquip(self, function_memory:FunctionMemory):
+        ...
+    def onUnequip(self, function_memory:FunctionMemory):
+        ...
+    def onDamaged(self, function_memory:FunctionMemory):
+        ...
+    def onBroken(self, function_memory:FunctionMemory):
+        ...
+
+    def _get_save(self, function_memory:FunctionMemory):
+        d = {
+            "type": "engine:armor",
+            "parent": self.abstract.identifier.full()
+        }
+
+        if self.name != self.abstract.getName():
+            d.update({"name": self.name})
+        if self.damage_reduction != self.abstract.getDamageReduction():
+            d.update({"damage_reduction": self.damage_reduction})
+        if self.max_durability != self.abstract.getMaxDurability():
+            d.update({"max_durability": self.max_durability})
+        if self.durability != self.abstract.getDurability():
+            d.update({"durability": self.durability})

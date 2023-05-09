6f08c291218dc6512fb0a5d42a7371164dc87d51
Weston Day <82739021+westbot657@users.noreply.github.com>
2023-05-09 14:53:09 +0000

    Had to add the non-generator evaluateFunction method back. it's used by DynamicValue, which would be too difficult to link to the generator chain

--- Engine/Resources/DynamicValue.py
+++ Engine/Resources/DynamicValue.py
@@ -40,16 +40,7 @@ class DynamicValue(FunctionalElement):
         returns the result of passing this value's raw_data to `DungeonLoader.evaluateFunction()`
         """
         if isinstance(self.raw_data, dict):
-            ev = self.cached_value = function_memory.generatorEvaluateFunction(self.raw_data)
-            v = None
-            try:
-                v = ev.send(None)
-                while isinstance(v, _EngineOperation):
-                    res = yield v
-                    v = ev.send(res)
-            except StopIteration as e:
-                v = e.value or (v if not isinstance(v, _EngineOperation) else None)
-            self.cached_value = v
+            self.cached_value = function_memory.evaluateFunction(self.raw_data)
         else:
             self.cached_value = self.raw_data
             
@@ -67,16 +58,9 @@ class DynamicValue(FunctionalElement):
         returns the cached value.
         if the cached value is None, makes a call to `getNew()` first.
         """
+
         if self.cached_value is None:
-            ev = self.getNew(function_memory)
-            v = None
-            try:
-                v = ev.send(None)
-                while isinstance(v, _EngineOperation):
-                    res = yield v
-                    v = ev.send(res)
-            except StopIteration as e:
-                v = e.value or (v if not isinstance(v, _EngineOperation) else None)
+            self.getNew(function_memory)
             
         return self.cached_value
 

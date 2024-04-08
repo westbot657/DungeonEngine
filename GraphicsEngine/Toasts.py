c3a18317451b6bfb0fa057191920d5ffda549b17
Weston Day <82739021+westbot657@users.noreply.github.com>
2024-04-08 15:44:49 -0600

    set up basic methods

--- GraphicsEngine/Toasts.py
+++ GraphicsEngine/Toasts.py
@@ -2,10 +2,29 @@
 
 from UIElement import UIElement
 
-class Toasts(UIElement):
-    def __init__(self):
-        pass
+import time
 
+
+class Toasts(UIElement):
+    def __init__(self, x, y, width):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.appearing = []
+        self.toasts = []
+        self.disappearing = []
+        
+
+
+    def toast(self, text:str, display_time:float=2):
+        ...
+    
+    
+    def _event(self, editor, X, Y):
+        ...
+    
+    def _update(self, editor, X, Y):
+        ...
 
 
 

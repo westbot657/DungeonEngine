2ab2f814340254419a8117a48b797fdb0c3b0166
westbot <82739021+westbot657@users.noreply.github.com>
2024-09-05 01:29:58 -0600

    working on assembler now I guess

--- Engine/Resources/ESInstructions.py
+++ Engine/Resources/ESInstructions.py
@@ -12,13 +12,33 @@ class Data:
             return f"N:{self.data}"
         elif isinstance(self.data, str):
             return f"S:{self.data!r}"
-        
+
+class Register:
+    _id = 0
+    def __init__(self, id):
+        self.id = id
+    
+    def __repr__(self):
+        return f"R:{self.id}"
+    
+    @classmethod
+    def new(cls):
+        r = cls(cls._id)
+        cls._id += 1
+        return r
+
+class MemoryObject:
+    def __init__(self, name):
+        self.name = name
+    
+    def __repr__(self):
+        return f"M:{self.name}"
 
 class Instruction:
     def __init__(self):
-        ...
+        self.line_number = -1
 
-    def assemble(self):
+    def assemble(self, line_number):
         raise Exception(f"assemble() not implemented for {self}")
 
 class GetInputI(Instruction):
@@ -27,58 +47,131 @@ class GetInputI(Instruction):
         self.prompt = prompt
         self.user = user
     
-    def assemble(self):
+    def assemble(self, line_number):
         return f"INPUT {self.store_to} {self.user} {self.prompt!r}"
 
 class BinOpI(Instruction):
-    def __init__(self, left, op, right):
+    def __init__(self, store_to, left, op, right):
+        self.store_to = store_to
         self.op = op
         self.left = left
         self.right = right
+        self.line_number = -1
 
-    def assemble(self):
-        return f"OP {self.op} {self.left} {self.right}"
+    def assemble(self, line_number):
+        self.line_number = line_number
+        return f"OP {self.store_to} {self.left} {self.op} {self.right}"
 
 class StoreI(Instruction):
     def __init__(self, store_to, value):
         self.store_to = store_to
         self.value = value
-    
-    def assemble(self):
+        self.line_number = -1
+
+    def assemble(self, line_number):
+        self.line_number = line_number
         return f"STORE {self.store_to} {self.value}"
 
-class PushStackI(Instruction):
-    def __init__(self):
-        pass
+# class PushStackI(Instruction):
+#     def __init__(self):
+#         self.line_number = -1
 
-    def assemble(self):
-        return "PUSHSTACK"
+#     def assemble(self, line_number):
+#         self.line_number = line_number
+#         return "PUSHSTACK"
 
-class PopStackI(Instruction):
-    def __init__(self): pass
-    
-    def assemble(self):
-        return "POPSTACK"
+# class PopStackI(Instruction):
+#     def __init__(self):
+#         self.line_number = -1
+
+#     def assemble(self, line_number):
+#         self.line_number = line_number
+#         return "POPSTACK"
 
 class OutputI(Instruction):
     def __init__(self, out):
         self.out = out
-    
-    def assemble(self):
+        self.line_number = -1
+
+    def assemble(self, line_number):
+        self.line_number = line_number
         return f"OUT {self.out}"
 
 class PrepArgsI(Instruction):
-    def __init__(self): pass
-    
-    def assemble(self):
+    def __init__(self):
+        self.line_number = -1
+
+    def assemble(self, line_number):
+        self.line_number = line_number
         return "PREPARGS"
+
+class SetArgI(Instruction):
+    def __init__(self, arg_name, value):
+        self.line_number = -1
+        self.arg_name = arg_name
+        self.value = value
+    
+    def assemble(self, line_number):
+        self.line_number = line_number
+        return f"SETARG {self.arg_name} {self.value}"
+
+class CallI(Instruction):
+    def __init__(self, call_to):
+        self.line_number = -1
+        self.call_to = call_to
+    
+    def assemble(self, line_number):
+        self.line_number = line_number
+        return f"CALL {self.call_to}"
+
+class GotoI(Instruction):
+    def __init__(self, target:Instruction):
+        self.target = target
+        while isinstance(self.target, GotoI):
+            self.target = self.target.target
+        self.line_number = -1
+
+    def assemble(self, line_number):
+        self.line_number = line_number
+        if (self.target.line_number != -1):
+            return f"GOTO {self.target.line_number}"
+        return self
 
 class LoadI(Instruction):
     def __init__(self, load_val, load_to):
         self.load_val = load_val
         self.load_to = load_to
-    
-    def assemble(self):
+        self.line_number = -1
+
+    def assemble(self, line_number):
+        self.line_number = line_number
         return f"LOAD {self.load_to} {self.load_val}"
 
+class NewI(Instruction):
+    def __init__(self, store_to, object_type):
+        self.store_to = store_to
+        self.object_type = object_type
+        self.line_number = -1
+    
+    def assemble(self, line_number):
+        self.line_number = line_number
+        return f"NEW {self.store_to} {self.object_type}"
 
+class WaitI(Instruction):
+    def __init__(self, duration):
+        self.duration = duration
+        self.line_number = -1
+    
+    def assemble(self, line_number):
+        self.line_number = line_number
+        return f"WAIT {self.duration}"
+
+class MoveI(Instruction):
+    def __init__(self, entity, destination):
+        self.entity = entity
+        self.destination = destination
+        self.line_number = -1
+    
+    def assemble(self, line_number):
+        self.line_number = line_number
+        return f"MOVE {self.entity} {self.destination}"
--- Engine/Resources/ESInterpreter.py
+++ Engine/Resources/ESInterpreter.py
@@ -2,13 +2,33 @@
 
 try:
     from .Logger import Log
+    from .ESInstructions import *
 except ImportError as e:
     from Logger import Log
+    from ESInstructions import *
 
 
 from mergedeep import merge
 from typing import Any
 from enum import Enum, auto
+
+class DataStack:
+    
+    def __init__(self):
+        self.stack: list[dict] = []
+        self.index = -1
+    
+    def push(self):
+        self.stack.append({})
+        self.index += 1
+        
+    def pop(self):
+        out = self.stack.pop(self.index)
+        self.index -= 1
+        return out
+    
+    def update(self, data:dict):
+        self.stack[self.index].update(data)
 
 class ESInterpreter:
     
@@ -42,7 +62,50 @@ class ESInterpreter:
             "#vars": {},
             "#data": {}
         }
+        self.pointer = 0
+        self.instructions = []
 
+    def assemble(self):
+        self.instructions.clear()
+        self._assemble(self.script)
+
+    def _assemble(self, branch):
+        if isinstance(branch, dict):
+            if funcs := branch.get("#functions", None):
+                self._assemble(funcs)
+            elif br := branch.get("#access", None):
+                ...
+            elif br := branch.get("#call", None):
+                ...
+            elif br := branch.get("#check", None):
+                ...
+            elif br := branch.get("#for", None):
+                ...
+            elif br := branch.get("#while", None):
+                ...
+            elif br := branch.get("#match", None):
+                ...
+            elif br := branch.get("#function", None):
+                ...
+            elif br := branch.get("#break", None):
+                ...
+            elif br := branch.get("#return", None):
+                ...
+            elif br := branch.get("#ref", None):
+                ...
+            elif br := branch.get("#store", None):
+                ...
+            elif br := branch.get("#not", None):
+                ...
+            elif br := branch.get("#new", None):
+                ...
+            elif br := branch.get("#move", None):
+                ...
+        elif isinstance(branch, list):
+            for item in branch:
+                self._assemble(item)
+        else:
+            pass # ignore invalid data
 
 
 
